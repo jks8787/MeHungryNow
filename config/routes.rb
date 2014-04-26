7929f1ba27d896e0f91a8fa8604773d5a1c5569a
Janice Smith <janice.k.n.smith@gmail.com>
2014-04-26 15:42:43 -0400

    setting up for choosing a fit goal and setting it to a user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 MeHungryNow::Application.routes.draw do
   devise_for :users
   root to: 'home#index'
-
+  get '/users/fit_goals', to: 'users#fit_goals', as: 'fit_goals'
   resources :cookbooks
 end
