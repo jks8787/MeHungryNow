1c037e42213783895c034efb9b2d9063acf4f7da
Jon Cheng <jon.syndicate@gmail.com>
2014-04-26 19:47:11 -0400

    working constant contact api implementation

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ MeHungryNow::Application.routes.draw do
   devise_for :users
   root to: 'home#index'
   get '/emails/getkey', to: 'emails#get_key'
-  post '/emails/postcontact', to: 'emails#postcontact'
+  get '/emails/posted_contact', to: 'emails#postcontact'
   get '/users/fit_goals', to: 'users#fit_goals', as: 'fit_goals'
   resources :cookbooks
   resources :users
