9b6a4d3e8062177dcd01b090acc89eb83f487cf2
Janice Smith <janice.k.n.smith@gmail.com>
2014-04-27 12:12:12 -0400

    recipe page

--- app/controllers/cookbooks_controller.rb
+++ app/controllers/cookbooks_controller.rb
@@ -2,13 +2,13 @@ class CookbooksController < ApplicationController
   def index
   end
 
-  def lifter
+  def runner
     api_id = ENV['yummly_app_id']
     api_key = ENV['yummly_app_key']
 
-    lifter_url = "http://api.yummly.com/v1/api/recipes?_app_id=#{api_id}&_app_key=#{api_key}&q=&nutrition.PROCNT.min=6&nutrition.PROCNT.max=8&nutrition.CHOCDF.max=8&nutrition.FASAT.max=4"
+    runner_url = "http://api.yummly.com/v1/api/recipes?_app_id=#{api_id}&_app_key=#{api_key}&q=&nutrition.PROCNT.min=6&nutrition.PROCNT.max=8&nutrition.CHOCDF.min=10&nutrition.FASAT.max=4"
 
-    @lifter_response = HTTParty.get(lifter_url)
+    @lifter_response = HTTParty.get(runner_url)
 
     render json: @lifter_response
   end
@@ -28,11 +28,11 @@ class CookbooksController < ApplicationController
     api_id = ENV['yummly_app_id']
     api_key = ENV['yummly_app_key']
 
-    yogi_url = "http://api.yummly.com/v1/api/recipes?_app_id=#{api_id}&_app_key=#{api_key}&q=&nutrition.PROCNT.min=4&nutrition.PROCNT.max=8&nutrition.CHOCDF.min=4&nutrition.CHOCDF.max=10&nutrition.FASAT.min=4&nutrition.FASAT.max=8"
+    liffter_url = "http://api.yummly.com/v1/api/recipes?_app_id=#{api_id}&_app_key=#{api_key}&q=&nutrition.PROCNT.min=6&nutrition.PROCNT.max=10&nutrition.CHOCDF.min=4&nutrition.CHOCDF.max=10&nutrition.FASAT.min=4&nutrition.FASAT.max=8"
 
-    @yogi_response = HTTParty.get(yogi_url)
+    @liffter_url = HTTParty.get(liffter_url)
 
-    render json: @yogi_response
+    render json: @liffter_url
   end
 
 end
